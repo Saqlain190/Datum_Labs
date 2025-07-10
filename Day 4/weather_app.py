672053d3710b7e982f479d1226930b11708509da
Saqlain Ahmad <saqlain@example.com>
2025-07-10 17:01:33 +0500

    Weather App

---
+++ Day 4/weather_app.py
@@ -0,0 +1,31 @@
+import requests
+
+def weather_app(city_name, api_key):
+    base_url = "https://api.openweathermap.org/data/2.5/weather"
+    
+    params = {
+        "city": city_name,
+        "appid": api_key,
+        "units": "metric"
+    }
+
+    response = requests.get(base_url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        temp = data["main"]["temp"]
+        humidity = data["main"]["humidity"]
+        description = data["weather"][0]["description"]
+
+        print(f"\nWeather in {city_name.title()}:")
+        print(f"Temperature: {temp}°C")
+        print(f"Humidity: {humidity}%")
+        print(f"Description: {description.capitalize()}")
+    else:
+        print("Error fetching data. Please check the city name or API key.")
+
+if __name__ == "__main__":
+    api_key = "aa1e040a558d0fc18fad77b6d4eb2b07"
+    city = input("Enter city name: ")
+    weather_app(city, api_key)
